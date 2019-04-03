6ac1fa0ef92d07a7a8d9ae53b77009f31c37f4f4
I.Miladinovic@student.han.nl <GGWPs@users.noreply.github.com>
2019-04-03 14:17:59 +0200

    cleaned

--- src/main/java/nl/han/oose/DAO/AccountDAO.java
+++ src/main/java/nl/han/oose/DAO/AccountDAO.java
@@ -1,14 +1,10 @@
 package nl.han.oose.DAO;
 
 import nl.han.oose.ConnectionFactory;
-
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.stream.IntStream;
 
 
 public class AccountDAO {
@@ -19,18 +15,18 @@ public class AccountDAO {
         connectionFactory = new ConnectionFactory();
     }
 
-    public boolean accountValidation(String username, String password) {
-        try (Connection connection = connectionFactory.getConnection();
-             PreparedStatement statement = connection.prepareStatement("SELECT * FROM account WHERE username = ? AND password = ?")) {
-            statement.setString(1, username);
-            statement.setString(2, password);
-            ResultSet resultSet = statement.executeQuery();
-            while (resultSet.next()) {
-                return true;
+        public boolean accountValidation(String username, String password) {
+            try (Connection connection = connectionFactory.getConnection();
+                 PreparedStatement statement = connection.prepareStatement("SELECT * FROM account WHERE username = ? AND password = ?")) {
+                statement.setString(1, username);
+                statement.setString(2, password);
+                ResultSet resultSet = statement.executeQuery();
+                while (resultSet.next()) {
+                    return true;
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
             }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+            return false;
         }
-        return false;
-    }
 }
