a5001a01d07d8fc42ea6e4846057e0bdaa721b53
I.Miladinovic@student.han.nl <GGWPs@users.noreply.github.com>
2019-04-03 14:18:38 +0200

    cleaned

--- src/main/java/nl/han/oose/DAO/AccountDAO.java
+++ src/main/java/nl/han/oose/DAO/AccountDAO.java
@@ -27,20 +27,6 @@ public class AccountDAO {
         connectionFactory = new ConnectionFactory();
     }
 
-//    public boolean accountValidation(String username, String password) {
-//        try (Connection connection = connectionFactory.getConnection();
-//             PreparedStatement statement = connection.prepareStatement("SELECT * FROM account WHERE username = ? AND password = ?")) {
-//            statement.setString(1, username);
-//            statement.setString(2, password);
-//            ResultSet resultSet = statement.executeQuery();
-//            while (resultSet.next()) {
-//                return true;
-//            }
-//        } catch (SQLException e) {
-//            throw new RuntimeException(e);
-//        }
-//        return false;
-//    }
         public boolean accountValidation(String username, String password) {
                 MongoDatabase database = connectionFactory.getDatabase();
                 MongoCollection<Document> collection = database.getCollection("account");
