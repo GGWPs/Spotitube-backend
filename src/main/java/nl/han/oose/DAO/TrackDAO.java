6d5cbdc64b403d7a5a79a84a0714d46b52d82d3a
Can Ugurlu <can.ugurlu@live.nl>
2021-05-27 00:29:48 +0200

    getAllTracks for Neo4j

--- src/main/java/nl/han/oose/DAO/TrackDAO.java
+++ src/main/java/nl/han/oose/DAO/TrackDAO.java
@@ -4,12 +4,20 @@ package nl.han.oose.DAO;
 import nl.han.oose.ConnectionFactory;
 import nl.han.oose.dto.Track;
 import nl.han.oose.dto.Tracks;
+import org.neo4j.driver.Record;
+import org.neo4j.driver.Result;
+import org.neo4j.driver.Session;
+import org.neo4j.driver.Transaction;
+import org.neo4j.driver.TransactionWork;
 
 import javax.inject.Inject;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Map;
+
+import static org.neo4j.driver.Values.parameters;
 
 public class TrackDAO {
 
@@ -20,20 +28,27 @@ public class TrackDAO {
 
     public Tracks getAllTracks(int playlistId) {
         Tracks tracks = new Tracks();
-
-        try (Connection connection = connectionFactory.getConnection();
-                PreparedStatement statement = connection.prepareStatement("SELECT * FROM track INNER JOIN tracksinplaylist ON id = track_id WHERE playlist_id = ?")) {
-            statement.setInt(1, playlistId);
-            ResultSet resultSet = statement.executeQuery();
-            while (resultSet.next()) {
-                try {
-                    tracks = trackInfo(resultSet);
-                } catch (Exception e) {
-                    System.out.println(e);
-                }
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+        try (Session session = connectionFactory.getNeo4JConnection().session() )
+        {
+            tracks = session.readTransaction(transaction -> {
+                    Result result = transaction.run( "MATCH (p:Playlist {id: $playlistId})-[:CONTAINS]->(t:Track) return t.id, t.title, t.performer, t.duration, t.album, t.playcount, t.publicationDate, t.description, t.offlineAvailable",
+                            parameters( "playlistId", playlistId));
+                    Tracks tempTracks = new Tracks();
+                    while (result.hasNext()) {
+                        Record record = result.next();
+                        Track track = new Track(record.get(0).asInt(),
+                                record.get(1).asString(),
+                                record.get(2).asString(),
+                                record.get(3).asInt(),
+                                record.get(4).asString(),
+                                record.get(5).asInt(),
+                                record.get(6).asLocalDate() + "",
+                                record.get(7).asString(),
+                                record.get(8).asBoolean());
+                        tempTracks.getTracks().add(track);
+                    }
+                    return tempTracks;
+                });
         }
         return tracks;
     }
